95b4834db377edb1485070211abe9ab5443ad356
IshaT-Hub <ishatiwari0508@gmail.com>
2025-04-08 17:58:57 +0530

    TabFormComponent

--- TabFormComponent/src/components/TabForm.jsx
+++ TabFormComponent/src/components/TabForm.jsx
@@ -1,6 +1,32 @@
-import '../style.css';
-export default function TabForm(){
-return(
-    <div>TabForm</div>
-);
+import Setting from "./Setting";
+import Profile from "./Profile";
+import Interest from "./Interest";
+
+import "../style.css";
+export default function TabForm() {
+	const tabs = [
+		{
+			name: "Profile",
+			component: Profile,
+		},
+		{
+			name: "Setting",
+			component: Setting,
+		},
+		{
+			name: "Interest",
+			component: Interest,
+		},
+	];
+	return (
+		<div>
+			<div className="heading-container">
+				{tabs.map((t) => (
+					<div className="heading" key={t.name}>
+						{t.name}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
