acad2c1380942c37f7f46623a00a10ddd9daaf7c
IshaTHub <ishatiwari0508@gmail.com>
2025-04-08 23:47:39 +0530

    Active Tabs

--- TabFormComponent/src/components/TabForm.jsx
+++ TabFormComponent/src/components/TabForm.jsx
@@ -3,30 +3,41 @@ import Profile from "./Profile";
 import Interest from "./Interest";
 
 import "../style.css";
+import { useState } from "react";
 export default function TabForm() {
-	const tabs = [
-		{
-			name: "Profile",
-			component: Profile,
-		},
-		{
-			name: "Setting",
-			component: Setting,
-		},
-		{
-			name: "Interest",
-			component: Interest,
-		},
-	];
-	return (
-		<div>
-			<div className="heading-container">
-				{tabs.map((t) => (
-					<div className="heading" key={t.name}>
-						{t.name}
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabs = [
+    //config driven ui
+    {
+      name: "Profile",
+      component: Profile,
+    },
+    {
+      name: "Setting",
+      component: Setting,
+    },
+    {
+      name: "Interest",
+      component: Interest,
+    },
+  ];
+
+  const ActiveTabComponent = tabs[activeTab].component;
+  return (
+    <div>
+      <div className="heading-container">
+        {tabs.map((t, index) => (
+          <div className="heading" key={t.name} onClick={() => setActiveTab(index)}> 
+		  {/* added callback to avoid immediate call to setActiveTab as it will not
+		   wait for the click event,causing infinite re-renders */}
+            {t.name}
+          </div>
+        ))}
+      </div>
+      <div className="tab-body">
+        <ActiveTabComponent />
+      </div>
+    </div>
+  );
 }
